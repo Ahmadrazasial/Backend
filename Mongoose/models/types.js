39454ebfcfc58fbaeee16e3ea15198de2c73e6a7
Ahmadrazasial <ahmadrazauch15@gmail.com>
2025-12-01 18:59:33 +0500

    objectid discussed

--- Mongoose/models/types.js
+++ Mongoose/models/types.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Types } from "mongoose";
 
 let dateschema = new mongoose.Schema({
     date:{type:String,required:true},
@@ -17,7 +17,47 @@ let dateschema = new mongoose.Schema({
        
 //     })
 
+let bufferSchema = new mongoose.Schema({
+    filename:String,
+    filedata:Buffer,
+
+})
+
+let mixedSchema = new mongoose.Schema({
+    name:String,
+    working:mongoose.Schema.Types.Mixed
+
+})
+
+const unionSchema = new mongoose.Schema({
+    values:{type:Schema.Types.Union,of:[String,Number,Array]}
+})
 
 
+const objectSchema = new mongoose.Schema({
+    bestFriend:{type:Schema.Types.ObjectId,unique:true,ref:"friend"}
+})
+
+
+const friendSchema = new mongoose.Schema({
+    name:String,
+    friend:Boolean,
+})
+
+const carSchema = new mongoose.Schema({
+    driver:mongoose.ObjectId
+})
+
+
+
+const file = mongoose.model('file',bufferSchema);
+const mixed = mongoose.model('mix',mixedSchema);
 const date = mongoose.model('birth',dateschema);
-export{date}
+const union = mongoose.model('union',unionSchema);
+const friend = mongoose.model('friend',friendSchema);
+const obj = mongoose.model('bestfriend',objectSchema);
+const car = mongoose.model('car',carSchema);
+export{date,file,mixed,union,obj,car,friend}
+
+
+
--- Mongoose/typesSchema.js
+++ Mongoose/typesSchema.js
@@ -1,6 +1,10 @@
 
 import mongoose from "mongoose";
-import { date } from './models/types.js';
+import { date,file,mixed,union,obj ,car,friend} from './models/types.js';
+import  fs  from "fs";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 await mongoose.connect('mongodb://localhost:27017/schematypes');
 const dateToday = new Date(Date.now())
@@ -33,4 +37,88 @@ const dup = await date.aggregate([
 const get = date.find({}).cursor()
 for await (const item of get) {
 console.log(item.date)    
+}
+ get.forEach(element => {
+  console.log(element)
+});
+
+
+// const imagebfr = fs.readFileSync("result.jpg")
+// console.log(imagebfr);
+// const bfrfiles = await file.create({
+//   filename:"result.jpg",
+//   filedata:imagebfr,
+
+// }) 
+// console.log(bfrfiles);
+
+const bfrread = await file.findOne();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const imagepath = path.join(__dirname,'output.jpg')
+
+ fs.writeFileSync(imagepath,bfrread.filedata);
+
+ console.log(imagepath)
+
+
+const app = express()
+const port = 3000
+
+// app.get('/:name',async(req,res)=>{
+
+//  const profile =  await mixed.create({
+//     name:req.params.name,
+//     working:["teacher","developer"]
+
+//   })
+// res.send(req.params.name + "'s profile is created")
+// })
+
+app.get('/img', (req, res) => {
+  
+  res.set('Content-Type','image/jpeg');
+  res.send(bfrread.filedata)
+  console.log()
+})
+
+app.get('/image', (req, res) => {
+  
+res.sendFile(imagepath);
+
+})
+
+app.get('/u', async(req, res) => {
+  
+await union.create({
+  values:15
+})
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+// const car = new car();
+car.driver = new mongoose.Types.ObjectId();
+
+console.log(typeof car.driver)
+// console.log(car.driver.toString());
+
+
+// await friend.updateMany({},{$unset:{name:""}})
+
+// await obj.create({
+//   bestFriend:"692d80e5cb74fb11915ada45",
+// })
+
+// await obj.deleteMany();
+
+const besti = await obj.find().populate('bestFriend')
+console.log(besti)
+
+for (const best of besti) {
+  console.log(best.bestFriend._id)
 }
