cfb1e0c8b07f0a8b85fd57cf91a406d04581db78
Ahmadrazasial <ahmadrazauch15@gmail.com>
2025-12-26 21:37:58 +0500

    Did exeercise using tailwind

---
+++ Layout design suing tailwind node/tailwind.config.js
@@ -0,0 +1,13 @@
+// const { theme, plugins, content } = require("../Tailwind cli/tailwind.config");
+
+module.exports = {
+    content:["*.html"],
+    theme:{
+        extend:{
+            fontFamily:{
+                times:['"Times New Roman"','Times','Serif'],
+            },
+        },
+    },
+    plugins:[]
+}
